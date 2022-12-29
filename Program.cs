d0a558ee07267fe7406c3f8b417a99d3a172bd4c
amiraelsayed44 <121624860+amiraelsayed44@users.noreply.github.com>
2022-12-29 20:55:37 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,37 @@
 ﻿using System;
 
-namespace perfect_numbers
+namespace prime_numbers
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            
+        { 
             Console.WriteLine(" please enter the first number");
             int n1 = int.Parse(Console.ReadLine());
             Console.WriteLine(" please enter the second number");
             int n2 = int.Parse(Console.ReadLine());
             for (int x = n1; x <= n2; x++)
             {
-                int sum = 0;
-                for (int i = 1;i<x; i++)
+                bool prime = true;
+                for (int i = 2; i < x; i++)
                 {
                     if (x % i == 0)
-                    
-                        sum=sum + i;
-                  
+                    {
+                        prime = false;
+                        
+                    }
 
                 }
+                if (prime)
+                    Console.WriteLine(x + " is prime number");
 
-                if (x == sum)
-                
-                    Console.WriteLine(x);
-                
+
             }
+
+           
+
+               
+            
         }
     }
 }
